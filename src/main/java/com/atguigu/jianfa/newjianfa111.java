a00841d5ca9689875e6f387ce3a234c8bf9a4cf6
linghuchong110 <1245920053@qq.com>
2021-12-16 23:35:53 +0800

    xinjianfa111

--- src/main/java/com/atguigu/jianfa/newjianfa111.java
+++ src/main/java/com/atguigu/jianfa/newjianfa111.java
@@ -3,5 +3,11 @@ package com.atguigu.jianfa;
 public class newjianfa111 {
     public static void main(String[] args) {
         System.out.println("新剑法");
+        System.out.println("新剑法");
+        System.out.println("新剑法");
+        System.out.println("新剑法");
+        System.out.println("新剑法");
+
+
     }
 }
