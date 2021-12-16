c210a22b8358815f146cd38804b0a20bace05872
linghuchong110 <1245920053@qq.com>
2021-12-16 22:57:20 +0800

    令狐冲修改

--- src/main/java/com/atguigu/jianfa/Dugu1.java
+++ src/main/java/com/atguigu/jianfa/Dugu1.java
@@ -3,5 +3,9 @@ package com.atguigu.jianfa;
 public class Dugu1 {
     public static void main(String[] args) {
         System.out.println("111111111");
+        System.out.println("22222");
+        System.out.println("22222");
+        System.out.println("22222");
+        System.out.println("22222");
     }
 }
